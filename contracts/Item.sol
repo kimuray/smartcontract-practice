e289a30f05d8fe34a2ff697999c5a5f4e4f4d969
Yoshihiro Kimura <kimuraysp@gmail.com>
2021-11-23 14:13:45 +0900

    ItemContractのコンストラクタを作成

--- contracts/Item.sol
+++ contracts/Item.sol
@@ -1,6 +1,28 @@
 pragma solidity ^0.8.10;
 
 contract Item {
+    string public name;
+    uint256 public price;
+    string public description;
+    string public imageURL;
+    bool public isSold;
+    address public owner;
+
+    constructor(
+        string memory _name,
+        uint256 _price,
+        string memory _description,
+        string memory _imageURL,
+        address _owner
+    ) public {
+        name = _name;
+        price = _price;
+        description = _description;
+        imageURL = _imageURL;
+        isSold = false;
+        owner = _owner;
+    }
+
     function sayHello() public pure returns (string memory value) {
         value = "Hello, World";
 
--- test/ItemContractTest.js
+++ test/ItemContractTest.js
@@ -1,11 +1,53 @@
 const ItemContract = artifacts.require("Item")
 
 contract("Item", (accounts) => {
+  let item
+  const name = "Tanaka Taro"
+  const price = 500
+  const description = "Very good item!!"
+  const imageURL = "http://example.com/hoge.png"
+  const owner = accounts[0]
+
+  beforeEach(async () => {
+    item = await ItemContract.new(name, price, description, imageURL, owner)
+  })
+
   describe("sayHello", () => {
     it("return Hello, world", async () => {
-      const item = await ItemContract.new()
       const actual = await item.sayHello()
       assert.equal(actual, "Hello, World", "words should match")
+    })
+  })
+
+  describe("initialize", () => {
+    it("gets the name", async () => {
+      const actual = await item.name()
+      assert.equal(actual, name, "name should match")
+    })
+
+    it("gets the description", async () => {
+      const actual = await item.description()
+      assert.equal(actual, description, "description should match")
+    })
+
+    it("gets the price", async () => {
+      const actual = await item.price()
+      assert.equal(actual, price, "price should match")
+    })
+
+    it("gets the imageURL", async () => {
+      const actual = await item.imageURL()
+      assert.equal(actual, imageURL, "imageURL should match")
+    })
+
+    it("gets the owner", async () => {
+      const actual = await item.owner()
+      assert.equal(actual, owner, "owner should match")
+    })
+
+    it("gets the isSold", async () => {
+      const actual = await item.isSold()
+      assert.equal(actual, false, "isSold should match")
     })
   })
 })
